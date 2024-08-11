012c1b3eddb9a1a88803c32a104dbeb471bd23cd
Fani-MA <misha.fani501@gmail.com>
2024-08-12 00:19:50 +0300

    Insert data

---
+++ src/main/resources/db.migration/V2_insert_user.sql
@@ -0,0 +1,16 @@
+INSERT INTO users(user_name, password, email, phone_number)
+VALUES ('misha', '2112', 'email@mail.com', '+77 88 55 467');
+
+INSERT INTO priority (level)
+VALUES  ('Low'),
+        ('Medium'),
+        ('High'),
+        ('Highest');
+
+
+INSERT INTO task_status (status)
+VALUES ('Created'),
+       ('In work'),
+       ('On review'),
+       ('Completed');
+
