65d0c73e033c181839eff05d7f29a5a4512d9955
Fani-MA <misha.fani501@gmail.com>
2024-08-12 00:19:22 +0300

    Create schema

--- src/main/resources/db.migration/V1_init.sql
+++ src/main/resources/db.migration/V1_init.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS users
 (
-    id            BIGSERIAL     PRIMARY KEY,
-    user_name     VARCHAR(30)  UNIQUE  not null ,
-    password      VARCHAR(50)   not null ,
-    email         VARCHAR(50) UNIQUE  not null ,
+    id            BIGSERIAL PRIMARY KEY,
+    user_name     VARCHAR(30) UNIQUE    NOT NULL ,
+    password      VARCHAR(50)           NOT NULL ,
+    email         VARCHAR(50) UNIQUE    NOT NULL ,
     phone_number  VARCHAR(20)
 );
 
@@ -11,11 +11,11 @@ CREATE TABLE IF NOT EXISTS users
 CREATE TABLE IF NOT EXISTS projects
 (
     id          BIGSERIAL PRIMARY KEY,
-    title       VARCHAR(50) NOT NULL ,
-    description VARCHAR(255) NOT NULL ,
-    state       VARCHAR(20) NOT NULL ,
-    created_at  TIMESTAMP WITH TIME ZONE NOT NULL,
-    author_id   BIGINT REFERENCES users(id) NOT NULL
+    title       VARCHAR(50)                     NOT NULL ,
+    description VARCHAR(255)                    NOT NULL ,
+    state       VARCHAR(20)                     NOT NULL ,
+    created_at  TIMESTAMP WITH TIME ZONE        NOT NULL ,
+    author_id   BIGINT REFERENCES users(id)     NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS task_status
@@ -25,21 +25,36 @@ CREATE TABLE IF NOT EXISTS task_status
 );
 
 
+CREATE TABLE IF NOT EXISTS project_task_states
+(
+    project_id  BIGINT REFERENCES projects(id) ,
+    task_status BIGINT REFERENCES task_status(id),
+    PRIMARY KEY (project_id, task_status)
+);
+
+CREATE TABLE IF NOT EXISTS priority
+(
+    id BIGSERIAL PRIMARY KEY ,
+    level VARCHAR(10)   NOT NULL
+);
+
 CREATE TABLE IF NOT EXISTS tasks (
     id            BIGSERIAL   PRIMARY KEY,
-    author_id     BIGINT REFERENCES users(id)  NOT NULL ,
-    executor_id   BIGINT REFERENCES users(id),
-    project_id    BIGINT REFERENCES projects(id) NOT NULL,
-    priority      VARCHAR(20) NOT NULL ,
-    created_at    TIMESTAMP WITH TIME ZONE NOT NULL ,
+    title         VARCHAR(100)                      NOT NULL ,
+    created_at    TIMESTAMP WITH TIME ZONE          NOT NULL ,
     start_time    TIMESTAMP WITH TIME ZONE,
     end_time      TIMESTAMP WITH TIME ZONE,
-    status_id     BIGINT REFERENCES task_status(id) NOT NULL
+    priority_id   BIGINT REFERENCES priority(id)    NOT NULL ,
+    status_id     BIGINT REFERENCES task_status(id) NOT NULL ,
+    author_id     BIGINT REFERENCES users(id)       NOT NULL ,
+    executor_id   BIGINT REFERENCES users(id),
+    project_id    BIGINT REFERENCES projects(id)    NOT NULL
 );
+
 
 CREATE TABLE IF NOT EXISTS comments
 (
     id         BIGSERIAL  PRIMARY KEY,
     message    VARCHAR(250),
-    task_id    BIGINT REFERENCES tasks(id) not null
+    task_id    BIGINT REFERENCES tasks(id) NOT NULL
 );
