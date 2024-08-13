220ac65eda2d1a871f6657ebbba0c7c9b9c9c027
Fani-MA <misha.fani501@gmail.com>
2024-08-14 00:57:14 +0300

    Clean

--- src/main/java/org/example/task/service/ProjectService.java
+++ src/main/java/org/example/task/service/ProjectService.java
@@ -1,19 +1,17 @@
 package org.example.task.service;
 
 import org.example.task.domain.project.ProjectDto;
-import org.example.task.domain.project.ProjectDtoFactory;
 import org.example.task.domain.project.ProjectResponse;
-import org.example.task.repository.ProjectRepository;
 
 import java.util.List;
 
 public interface ProjectService {
 
-    public List<ProjectDto> findAllProjects();
+    List<ProjectDto> findAllProjects();
 
-    public ProjectResponse getProject(Long id);
+    ProjectResponse getProject(Long id);
 
-    public String closeProject(Long projectId, Long userId);
+    String closeProject(Long projectId, Long userId);
 
-    public List<ProjectDto> findMyProjects(Long id);
+    List<ProjectDto> findMyProjects(Long id);
 }
