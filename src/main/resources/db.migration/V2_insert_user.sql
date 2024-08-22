2729971eb5e8dc2d53481b739602e8650e3f68c9
misha <misha.fani501@gmail.com>
2024-08-22 03:45:05 +0300

    Add data in migration(v2)

--- src/main/resources/db.migration/V2_insert_user.sql
+++ src/main/resources/db.migration/V2_insert_user.sql
@@ -1,19 +1,25 @@
 INSERT INTO users(user_name, password, email, phone_number)
-    VALUES ('misha', '2112', 'email@mail.com', '+77 88 55 467');
+    VALUES  ('misha', '2112', 'email@mail.com', '+77 88 55 467'),
+            ('pestonia', '2112', 'Pestonia@mail.com', '+77 88 63 854'),
+            ('kvasar', '2112', 'kvasar@mail.com', '+77 83 77 467'),
+            ('skywing', '2112', 'skywing@mail.com', '+77 85 64 497');
 
 INSERT INTO roles(role)
     VALUES ('ROLE_USER'),
            ('ROLE_ADMIN');
 
 INSERT INTO user_roles
-    VALUES (1, 1),
-           (1, 2);
+VALUES (1, 1),
+       (1, 2),
+       (2, 1),
+       (3, 1),
+       (4, 1);
 
 INSERT INTO priority (level)
     VALUES  ('Low'),
-        ('Medium'),
-        ('High'),
-        ('Highest');
+            ('Medium'),
+            ('High'),
+            ('Highest');
 
 
 INSERT INTO task_stage (stage)
@@ -22,3 +28,8 @@ INSERT INTO task_stage (stage)
            ('On review'),
            ('Completed');
 
+INSERT INTO projects(title, description, state, created_at, author_id)
+VALUES  ('My sample project','Study alg','ACTIVE','2024-08-15',1),
+        ('Second project','Work tasks','ACTIVE','2024-08-17',1),
+        ('Study project','OSI','ACTIVE','2024-08-12',2),
+        ('Everyday','Daily tasks','ACTIVE','2024-08-12',1);
