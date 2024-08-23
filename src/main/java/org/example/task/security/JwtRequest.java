0682b813180d0beb136eb635bfe5096b87059404
misha <misha.fani501@gmail.com>
2024-08-23 13:32:44 +0300

    Clean

--- src/main/java/org/example/task/security/JwtRequest.java
+++ src/main/java/org/example/task/security/JwtRequest.java
@@ -1,15 +1,16 @@
 package org.example.task.security;
 
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.experimental.FieldDefaults;
 
 import java.io.Serializable;
 
 @Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class JwtRequest implements Serializable {
-//    String username;
 
     String email;
 
     String password;
-
 }
