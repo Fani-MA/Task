d96c717b5743b02c0b07a0a83b1df3b88837faa5
misha <misha.fani501@gmail.com>
2024-08-23 16:44:43 +0300

    Fix CustomExceptionHandler

--- src/main/java/org/example/task/exeption/CustomExceptionHandler.java
+++ src/main/java/org/example/task/exeption/CustomExceptionHandler.java
@@ -4,19 +4,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.Date;
-import java.util.List;
-
 @RestControllerAdvice
 public class CustomExceptionHandler {
 
     @ExceptionHandler
     public ErrorDto projectNotFoundException(ProjectNotFoundException e){
-//        return ErrorDto.builder()
-//                .time(new Date())
-//                .status(HttpStatus.BAD_REQUEST)
-//                .message(e.getMessage())
-//                .build();
         return new ErrorDto(HttpStatus.BAD_REQUEST, e.getMessage());
     }
 }
