6e72669f3e5c2fbf272247ab22c427145c62746e
misha <misha.fani501@gmail.com>
2024-08-23 16:43:16 +0300

    Add validation

--- src/main/java/org/example/task/domain/user/UserRequest.java
+++ src/main/java/org/example/task/domain/user/UserRequest.java
@@ -1,14 +1,22 @@
 package org.example.task.domain.user;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class UserRequest {
 
+    @NotBlank(message = "Username must not be empty")
+    @Size(min = 4, message = "Min length username is 4 characters")
     String username;
 
+    @NotBlank(message = "Password must not be empty")
+    @Size(min = 4, message = "Min length password is 4 characters")
     String password;
 
+    @Email(message = "Uncorrected email")
     String email;
 
 }
--- src/main/java/org/example/task/security/JwtRequest.java
+++ src/main/java/org/example/task/security/JwtRequest.java
@@ -1,7 +1,11 @@
 package org.example.task.security;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.Data;
+import lombok.ToString;
 import lombok.experimental.FieldDefaults;
 
 import java.io.Serializable;
@@ -10,7 +14,11 @@ import java.io.Serializable;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class JwtRequest implements Serializable {
 
+    @Email(message = "Email must not be empty")
     String email;
 
+    @NotBlank(message = "Password must not be empty")
+    @Size(min = 4, message = "Password need contain min 4 characters")
+    @ToString.Exclude
     String password;
 }
