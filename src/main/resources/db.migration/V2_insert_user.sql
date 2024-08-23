ba4d9711d20e184666fcad83555755b85fe76230
misha <misha.fani501@gmail.com>
2024-08-23 12:38:54 +0300

    Change migration(v2)

--- src/main/resources/db.migration/V2_insert_user.sql
+++ src/main/resources/db.migration/V2_insert_user.sql
@@ -1,10 +1,10 @@
 INSERT INTO users(user_name, password, email, phone_number)
-    VALUES  ('misha', '2112', 'email@mail.com', '+77 88 55 467'),
-            ('pestonia', '2112', 'Pestonia@mail.com', '+77 88 63 854'),
-            ('kvasar', '2112', 'kvasar@mail.com', '+77 83 77 467'),
-            ('skywing', '2112', 'skywing@mail.com', '+77 85 64 497');
+    VALUES  ('misha', '$2a$10$4dWfhiBZRtQ/BrTn4mrpG.sOx0rW14qSdIx194deUqP5s0Y.MfwjO', 'email@mail.com', '+77 88 55 467'),
+            ('pestonia', '$2a$10$4dWfhiBZRtQ/BrTn4mrpG.sOx0rW14qSdIx194deUqP5s0Y.MfwjO', 'Pestonia@mail.com', '+77 88 63 854'),
+            ('kvasar', '$2a$10$4dWfhiBZRtQ/BrTn4mrpG.sOx0rW14qSdIx194deUqP5s0Y.MfwjO', 'kvasar@mail.com', '+77 83 77 467'),
+            ('skywing', '$2a$10$4dWfhiBZRtQ/BrTn4mrpG.sOx0rW14qSdIx194deUqP5s0Y.MfwjO', 'skywing@mail.com', '+77 85 64 497');
 
--- $2a$10$4dWfhiBZRtQ/BrTn4mrpG.sOx0rW14qSdIx194deUqP5s0Y.MfwjO
+-- $2a$10$4dWfhiBZRtQ/BrTn4mrpG.sOx0rW14qSdIx194deUqP5s0Y.MfwjO = 2112
 INSERT INTO roles(role)
     VALUES ('ROLE_USER'),
            ('ROLE_ADMIN');
